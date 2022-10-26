216ebc8eff79e61f3ee499eeaa2f3896565fbfea
lauromiottojr <junior.miotto36@gmail.com>
2022-10-26 13:27:42 -0300

    Listing Categoria with DTO (Data Transfer Object)

--- src/main/java/com/springbootionic/cursomc/services/CategoriaService.java
+++ src/main/java/com/springbootionic/cursomc/services/CategoriaService.java
@@ -1,6 +1,7 @@
 // classe que faz contato com a repo, aqui eu faço tratamento de exceções
 package com.springbootionic.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,5 +45,9 @@ public class CategoriaService {
 		} catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
