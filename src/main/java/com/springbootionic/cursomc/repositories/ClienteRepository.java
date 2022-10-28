369175092b340debd1cc273a85a950176b3eadef
lauromiottojr <junior.miotto36@gmail.com>
2022-10-28 11:08:19 -0300

    Custom validation Email unique

--- src/main/java/com/springbootionic/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/springbootionic/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,15 @@ package com.springbootionic.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springbootionic.cursomc.domain.Cliente;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
+	// o framework ja entendo com essa anotação e colocando "findBy..." que você quer buscar algo no banco
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
+
 }
--- src/main/java/com/springbootionic/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/springbootionic/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springbootionic.cursomc.domain.Cliente;
 import com.springbootionic.cursomc.domain.enums.TipoCliente;
 import com.springbootionic.cursomc.dto.ClienteNewDTO;
+import com.springbootionic.cursomc.repositories.ClienteRepository;
 import com.springbootionic.cursomc.resources.exception.FieldMessage;
 import com.springbootionic.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -24,9 +32,17 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido!"));
-		} if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())
+		} 
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())
 				&& !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente!"));
 		}
 
 		for (FieldMessage e : list) {
