23155a7e56955e8d6177889481f840d3043e5bd0
lauromiottojr <junior.miotto36@gmail.com>
2022-10-31 11:25:17 -0300

    toString Pedido

--- src/main/java/com/springbootionic/cursomc/services/PedidoService.java
+++ src/main/java/com/springbootionic/cursomc/services/PedidoService.java
@@ -34,6 +34,9 @@ public class PedidoService {
 
 	@Autowired
 	private ProdutoService produtoService;
+	
+	@Autowired
+	private ClienteService clienteService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -45,6 +48,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -54,11 +58,13 @@ public class PedidoService {
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
-			ip.setDesconto(0d);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
