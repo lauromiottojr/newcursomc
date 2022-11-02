316df500c73933467e7a5f4fc5f78236696e6bde
lauromiottojr <junior.miotto36@gmail.com>
2022-11-02 11:25:23 -0300

    Send img by endpoint

--- src/main/java/com/springbootionic/cursomc/resources/ClienteResource.java
+++ src/main/java/com/springbootionic/cursomc/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springbootionic.cursomc.domain.Cliente;
@@ -26,59 +27,67 @@ import com.springbootionic.cursomc.services.ClienteService;
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
-	
+
 	@Autowired
 	private ClienteService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-		
+
 		Cliente obj = service.find(id);
-		return ResponseEntity.ok().body(obj);		
-	}	
-	
-	//@RequestBody é para o JSON ser convertido para o obj (objeto)
-		@RequestMapping(method = RequestMethod.POST)
-		public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
-			Cliente obj = service.fromDTO(objDto);
-			obj = service.insert(obj);
-			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-			return ResponseEntity.created(uri).build();
-		}	
-	
+		return ResponseEntity.ok().body(obj);
+	}
+
+	// @RequestBody é para o JSON ser convertido para o obj (objeto)
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 	// @PathVariable é para receber o parametro na URL
-		@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-		public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
-			Cliente obj = service.fromDTO(objDto); // convertendo cliente em clienteDTO
-			obj.setId(id);
-			obj = service.update(obj);
-			return ResponseEntity.noContent().build();
-		}
-		
-		@PreAuthorize("hasAnyRole('ADMIN')") // somente ADMIN pode fazer
-		@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-		public ResponseEntity<Cliente> delete(@PathVariable Integer id) {		
-			service.delete(id);
-			return ResponseEntity.noContent().build();
-		}
-		
-		@PreAuthorize("hasAnyRole('ADMIN')") // somente ADMIN pode fazer
-		@RequestMapping(method = RequestMethod.GET)
-		public ResponseEntity<List<ClienteDTO>> findAll() {		
-			List<Cliente> list = service.findAll();
-			List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
-			return ResponseEntity.ok().body(listDto);		
-		}
-		
-		@PreAuthorize("hasAnyRole('ADMIN')") // somente ADMIN pode fazer
-		@RequestMapping(value = "/page", method = RequestMethod.GET)
-		public ResponseEntity<Page<ClienteDTO>> findPage(
-				@RequestParam(value = "page", defaultValue = "0") Integer page, 
-				@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, // @RequestParam é um parametro opcional passado na url, ex: ?page=x&lines=x...
-				@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
-				@RequestParam(value = "direction", defaultValue = "ASC") String direction) {		
-			Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
-			Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
-			return ResponseEntity.ok().body(listDto);		
-		}	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDto); // convertendo cliente em clienteDTO
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PreAuthorize("hasAnyRole('ADMIN')") // somente ADMIN pode fazer
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Cliente> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PreAuthorize("hasAnyRole('ADMIN')") // somente ADMIN pode fazer
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@PreAuthorize("hasAnyRole('ADMIN')") // somente ADMIN pode fazer
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, // @RequestParam é um
+																								// parametro opcional
+																								// passado na url, ex:
+																								// ?page=x&lines=x...
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value = "/picture", method = RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name = "file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
+	}
 }
